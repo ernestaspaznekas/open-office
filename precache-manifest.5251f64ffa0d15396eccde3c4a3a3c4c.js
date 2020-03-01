2a2c3d83c5bfb336969c89a1d2def787d281bfc5
Ernestas Paznekas <ernestas.paznekas@gmail.com>
2020-03-01 16:49:34 +0200

    Updates

--- precache-manifest.5251f64ffa0d15396eccde3c4a3a3c4c.js
+++ precache-manifest.5251f64ffa0d15396eccde3c4a3a3c4c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cce362f376767c43ea81fa16b4ce6cec",
+    "revision": "0cfec7cb2c95d9eb662049b1d2416677",
     "url": "/open-office/index.html"
   },
   {
-    "revision": "1e81d10f78d2dd0832f7",
-    "url": "/open-office/static/css/main.e12060b6.chunk.css"
+    "revision": "433cca7f93d57d975e08",
+    "url": "/open-office/static/css/main.718a91d1.chunk.css"
   },
   {
     "revision": "9d728840ec7b38085161",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/open-office/static/js/2.a2ce8c3b.chunk.js.LICENSE"
   },
   {
-    "revision": "1e81d10f78d2dd0832f7",
-    "url": "/open-office/static/js/main.6e02e5f5.chunk.js"
+    "revision": "433cca7f93d57d975e08",
+    "url": "/open-office/static/js/main.2ef2299f.chunk.js"
   },
   {
     "revision": "fcf5f049adb1d5b28819",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/open-office/precache-manifest.e4fdeb00b0a1d4471f902d64e3f434f0.js"
+  "/open-office/precache-manifest.5251f64ffa0d15396eccde3c4a3a3c4c.js"
 );
 
 self.addEventListener('message', (event) => {
