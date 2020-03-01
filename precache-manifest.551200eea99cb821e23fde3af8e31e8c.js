800521d58695c177883b06392f126c8774882648
Ernestas Paznekas <ernestas.paznekas@gmail.com>
2020-03-01 16:39:44 +0200

    Updates

---
+++ precache-manifest.551200eea99cb821e23fde3af8e31e8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c6f11dafaaab7c9a7db87dfe1b3fd7",
+    "url": "/open-office/index.html"
+  },
+  {
+    "revision": "d0c4c375f47e1f7e09b8",
+    "url": "/open-office/static/css/main.e12060b6.chunk.css"
+  },
+  {
+    "revision": "9d728840ec7b38085161",
+    "url": "/open-office/static/js/2.a2ce8c3b.chunk.js"
+  },
+  {
+    "revision": "5d83b32028c822b364148904e89bb0e7",
+    "url": "/open-office/static/js/2.a2ce8c3b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0c4c375f47e1f7e09b8",
+    "url": "/open-office/static/js/main.754b5192.chunk.js"
+  },
+  {
+    "revision": "fcf5f049adb1d5b28819",
+    "url": "/open-office/static/js/runtime-main.8121a457.js"
+  },
+  {
+    "revision": "ef2ca6651201fb12139144e49bda8363",
+    "url": "/open-office/static/media/cliff.ef2ca665.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/open-office/static/media/logo.5d5d9eef.svg"
+  }
+]);
